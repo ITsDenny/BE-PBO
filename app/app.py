f6e9950c6c4a87c71a65d74f6aedf65c5bb02de8
ITsDenny <dennym.channel@gmail.com>
2023-12-26 00:38:37 +0700

    script login

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
 from dotenv import load_dotenv
+import bcrypt
 import os
 
 app = Flask(__name__)
@@ -13,8 +14,6 @@ app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 MySQL = MySQL(app)
-
-
 
 @app.route("/")
 def index():
@@ -46,6 +45,34 @@ def data():
     except Exception as e:
         return jsonify(e)
 
+def check_password(password,hashed_password):
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
+
+def hash_password():
+    pass
+
+@app.route("/login", methods=['POST'])
+def login():
+    try:
+        email = request.json.get('email')
+        password = request.json.get('password')
+
+        cur = MySQL.connection.cursor()
+        cur.execute("SELECT password FROM user WHERE email = %s", (email,))
+        user = cur.fetchone()
+
+        if user and check_password(password, user[0]):
+            return jsonify({
+                'message' : 'Login succeeded',
+                'email' : email
+            }), 200
+        else:
+            return jsonify({'message' : 'Email atau password salah!'}), 401
+
+        cur.close()
+
+    except Exception as e:
+        return jsonify({'message': 'Error: ' + str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
